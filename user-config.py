75b298a3065b81b026b7872afa0279a5bfd36eb4
nlwikibots project <nlwikibots@willow.(none)>
2012-10-25 21:34:04 +0000

    "Initial" commit

---
+++ user-config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8  -*-
+family = 'wikipedia'
+mylang = 'nl'
+usernames['wikipedia']['nl'] = u'Valhallasw-toolserver-botje'
+
+userinterface_init_kwargs = {'default_stream': 'stdout'}
